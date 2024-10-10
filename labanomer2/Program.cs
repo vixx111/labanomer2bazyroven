f2b50540965ab41974557c90332c4261e698fa6c
Admin <Admin@DESKTOP-UGP0C2G>
2024-10-10 20:56:33 +0200

    Добавьте файлы проекта.

---
+++ labanomer2/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    double t = 2;
+    double m = 0.2;
+    Console.WriteLine("Введите A:");
+    double a = double.Parse(Console.ReadLine());
+    double w = 2 * Math.PI * Math.PI * Math.Sqrt(a) * Math.Sqrt(m) / Math.Sqrt(t);
+    Console.WriteLine($"Ответ {w}");
+
+}
+catch
+{
+    
+}
